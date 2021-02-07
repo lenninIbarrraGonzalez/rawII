b78a8d1a459668953663ca16d9a4459eb87e0ddb
Lennin666 <soporte@allnexus.com.co>
2021-02-06 19:05:10 -0500

    agregando botones de menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
 
+const useStyles = makeStyles(theme => ({
+  offset: theme.mixins.toolbar,
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1
+  }
 
-const Navbar = () => (
-  <div>
-    <AppBar position="fixed" color="primary">
-      <Toolbar>
-        <Typography variant="h6">
-          RAVII
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </div>
-)
+}))
+
+const Navbar = () => {
+  const classes = useStyles()
+  return (
+    <div>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <MenuIcon
+            aria-label="Menu"
+            className={classes.menuButton}
+          // onClick={() => actionOpen()}
+          />
+          <Typography variant="h6" className={classes.title}>
+            RAVII
+          </Typography>
+          <MenuIcon
+            aria-label="Menu"
+            className={classes.menuButton}
+          // onClick={() => actionOpen()}
+          />
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
+    </div>
+  )
+}
 
 export default Navbar;
