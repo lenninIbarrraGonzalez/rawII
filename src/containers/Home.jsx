551ae77373c1b9d6397ededd74db80a4a49b697a
Lennin666 <soporte@allnexus.com.co>
2021-02-08 21:07:35 -0500

    implementando diseño del buscador

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BannerHome from '../components/BannerHome';
 import Films from '../components/Films'
+import Search from '../components/Search';
 
 const Home = () => (
   <>
+    <Search />
     <BannerHome />
     <h1>Esto es el home</h1>
     <Films />
