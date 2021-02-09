9a9c7913fb533696b17cccfca4933f0a00e1fe47
Lennin666 <soporte@allnexus.com.co>
2021-02-08 20:57:20 -0500

    implementando el loading a las peliculas

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -7,7 +7,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
@@ -17,11 +16,12 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Animations from './Animations';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
-
   },
   media: {
     height: 0,
@@ -40,45 +40,72 @@ const useStyles = makeStyles((theme) => ({
   avatar: {
     backgroundColor: red[500],
   },
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 }));
+
+
 
 const useFetch = url => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(async () => {
     const response = await fetch(url);
     const data = await response.json();
     const item = data;
     setData(item);
+    setLoading(false);
   }, []);
 
-  return { data };
+  return { data, loading };
+
 };
 
 
 
 function Films() {
-  const { data } = useFetch("https://yts.mx/api/v2/list_movies.json");
+  const { data, loading } = useFetch("https://yts.mx/api/v2/list_movies.json");
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
+  if (loading) {
+    return (
+      <Container className={classes.container}>
+        <Grid container spacing={3} >
+          <Grid item xs md={2} lg={3}>
+            <Animations />
+          </Grid>
+          <Grid item xs md={2} lg={3}>
+            <Animations />
+          </Grid>
+          <Grid item xs md={2} lg={3}>
+            <Animations  />
+          </Grid>
+          <Grid item xs md={2} lg={3}>
+            <Animations />
+          </Grid>
+        </Grid>
+      </Container>
+    )
+  }
   const peliculas = data?.data.movies
   return (
-    <div>
-      <Grid container spacing={3}>
+    <Container className={classes.container}>
+      <Grid container spacing={3} className={classes.grid}>
         {peliculas?.map((pelicula) => (
-          <Grid item xs={3} key={pelicula.id}>
+          <Grid item xs md={2} lg={3} key={pelicula.id}>
 
             <Card className={classes.root}>
               <CardHeader
-                avatar={(
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    L
-                  </Avatar>
-                )}
                 action={(
                   <IconButton aria-label="settings">
                     <MoreVertIcon />
@@ -132,7 +159,7 @@ function Films() {
           </Grid>
         ))}
       </Grid>
-    </div>
+    </Container>
 
   )
 }
